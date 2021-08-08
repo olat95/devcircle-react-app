fed17ee55e3e56d385a5dff71166a9e1b6b0b402
Olatunji Oluwapelumi Samson <samsontunj@gmail.com>
2021-08-08 09:34:12 +0100

    corrected some style error

--- server/index.js
+++ server/index.js
@@ -1,73 +1,79 @@
-const express = require("express");
-const socket = require("socket.io");
-const http = require("http");
-const cors = require("cors");
-const moment = require("moment");
-const { addUser, getUser, removeUser, getUsersInRoom } = require("./users.js");
+const express = require('express')
+const socket = require('socket.io')
+const http = require('http')
+//  const cors = require('cors')
+// const moment = require('moment')
+const { addUser, getUser, removeUser, getUsersInRoom } = require('./users.js')
 
-const port = process.env.PORT || 5000;
-const hostname = "localhost";
+const port = process.env.PORT || 5000
+const hostname = 'localhost'
 
 corsOption = {
   cors: true,
-  origin: ["http://localhost:5000"],
-};
+  origin: ['http://localhost:5000'],
+}
 
-const router = require("./router");
+const router = require('./router')
 
-const app = express();
-const server = http.createServer(app);
-const io = socket(server, corsOption);
+const app = express()
+const server = http.createServer(app)
+const io = socket(server, corsOption)
 
-//.on means its listening for event from the frontend
-//.emit means its emitting data for the frond end to listen for
+//  .on means its listening for event from the frontend
+//  .emit means its emitting data for the frond end to listen for
 
-io.on("connection", (socket) => {
-  console.log("Starting chat on DevCircle platform");
+io.on('connection', (socket) => {
+  console.log('Starting chat on DevCircle platform')
 
-  socket.on("join", ({ name, room }, callback) => {
-    const { error, user } = addUser({ id: socket.id, name, room });
+  socket.on('join', ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room })
 
-    if (error) return callback(error);
+    if (error) return callback(error)
 
-    socket.emit("message", {
-      user: "admin",
+    socket.emit('message', {
+      user: 'admin',
       text: `${user.name}, welcome to this chatroom ${user.room}`,
-    });
+    })
 
     socket.broadcast
       .to(user.room)
-      .emit("message", { user: "admin", text: `${user.name}  has joined` });
+      .emit('message', { user: 'admin', text: `${user.name}  has joined` })
 
-    socket.join(user.room);
+    socket.join(user.room)
 
-    io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user.room)})
+    io.to(user.room).emit('roomData', {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    })
 
-    callback();
-  });
+    callback()
+  })
 
-  socket.on("sendMessage", (message, callback) => {
-    const user = getUser(socket.id);
-    io.to(user.room).emit("message", { user: user.name, text: message });
-    io.to(user.room).emit("roomData", { room: user.room, users: getUsersInRoom(user.room)});
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id)
+    io.to(user.room).emit('message', { user: user.name, text: message })
+    io.to(user.room).emit('roomData', {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    })
 
-    callback();
-  });
+    callback()
+  })
 
-  socket.on("disconnect", () => {
-    const user = removeUser(socket.id);
+  socket.on('disconnect', () => {
+    const user = removeUser(socket.id)
 
     if (user) {
-      io.to(user.room).emit("message", {
-        user: "admin",
+      io.to(user.room).emit('message', {
+        user: 'admin',
         text: `${user.name} left`,
-      });
+      })
     }
-  });
-});
+  })
+})
 
-app.use(router);
+app.use(router)
 
 server.listen(port, hostname, () => {
-  console.log(`Server listening at http://${hostname}:${port}`);
-});
+  console.log(`Server listening at http://${hostname}:${port}`)
+})
--- server/users.js
+++ server/users.js
@@ -1,32 +1,32 @@
-const users = [];
+const users = []
 
 const addUser = ({ id, name, room }) => {
-  name = name.trim().toLowerCase();
-  room = room.trim().toLowerCase();
+  name = name.trim().toLowerCase()
+  room = room.trim().toLowerCase()
 
   const existingUser = users.find(
     (user) => user.room === room && user.name === name
-  );
+  )
 
   if (existingUser) {
-    return { error: "Username already exists" };
+    return { error: 'Username already exists' }
   }
 
-  const user = { id, name, room };
-  users.push(user);
-  return { user };
-};
+  const user = { id, name, room }
+  users.push(user)
+  return { user }
+}
 
 const removeUser = (id) => {
-  const index = users.findIndex((user) => user.id === id);
+  const index = users.findIndex((user) => user.id === id)
 
   if (index !== -1) {
-    return users.splice(index, 1)[0];
+    return users.splice(index, 1)[0]
   }
-};
+}
 
-const getUser = (id) => users.find((user) => user.id === id);
+const getUser = (id) => users.find((user) => user.id === id)
 
-const getUsersInRoom = (room) => users.filter((user) => user.room === room);
+const getUsersInRoom = (room) => users.filter((user) => user.room === room)
 
-module.exports = { addUser, getUser, removeUser, getUsersInRoom };
+module.exports = { addUser, getUser, removeUser, getUsersInRoom }
